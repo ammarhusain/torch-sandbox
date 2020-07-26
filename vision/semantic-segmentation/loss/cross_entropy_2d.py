222def5b3aba2d29e847dd7b523ceecac6943614
ammarhusain <mrahusain@gmail.com>
2020-07-26 09:06:23 -0700

    Loss going down; memory consumption still very high

--- vision/semantic-segmentation/loss/cross_entropy_2d.py
+++ vision/semantic-segmentation/loss/cross_entropy_2d.py
@@ -1,11 +1,10 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 class CrossEntropy2d(nn.Module):
   def __init__(self, dim=1):
     super().__init__()
-    self.dim = dim
     self.criterion = nn.NLLLoss()
+    self.softmax = nn.LogSoftmax(dim=1)
   def forward(self, input, target):
-    return -1 * self.criterion(F.log_softmax(input, dim=self.dim), target)
+    return self.criterion(self.softmax(input), target)
   
